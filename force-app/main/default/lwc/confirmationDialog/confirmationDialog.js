4f755f6f8423e3a67ea72f464641ac82a93fce87
Akrom Saidkamolov <akrom@i2max.co.kr>
2022-09-12 01:35:11 +0900

    confirmation dialog: conditional css

--- force-app/main/default/lwc/confirmationDialog/confirmationDialog.js
+++ force-app/main/default/lwc/confirmationDialog/confirmationDialog.js
@@ -11,6 +11,16 @@ export default class ConfirmationDialog extends LightningElement {
     @api cancelLabel = "No"; // cancel button label
     @api response; // used to convey user's response to Flow
 
+    // getters
+    get dialogBodyClasses() {
+        const classes = "slds-modal__content slds-var-p-around_x-large";
+        if (this.showTitle) {
+            return classes;
+        }
+
+        return classes + " round-borders";
+    }
+
     //handles button clicks
     handleClick(event) {
         // get a boolean value of user's response.
