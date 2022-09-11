a4e27fb3913320821bec533a1a5db5f6b0265eea
Akrom Saidkamolov <akrom@i2max.co.kr>
2022-09-12 01:01:05 +0900

    custom confirmation dialog

--- force-app/main/default/lwc/confirmationDialog/confirmationDialog.js
+++ force-app/main/default/lwc/confirmationDialog/confirmationDialog.js
@@ -1,3 +1,29 @@
-import { LightningElement } from "lwc";
+import { LightningElement, api } from "lwc";
+import { FlowAttributeChangeEvent, FlowNavigationNextEvent } from "lightning/flowSupport";
 
-export default class ConfirmationDialog extends LightningElement {}
+export default class ConfirmationDialog extends LightningElement {
+    @api isFlow = false;
+    @api visible; // used to hide/show dialog
+    @api showTitle = false;
+    @api title = "Custom Dialog"; // modal title
+    @api message; // modal message
+    @api confirmLabel = "Yes"; // confirm button label
+    @api cancelLabel = "No"; // cancel button label
+    @api response; // used to convey user's response to Flow
+
+    //handles button clicks
+    handleClick(event) {
+        // get a boolean value of user's response.
+        // confirmation = true, rejection = false.
+        const userResponse = event.target.name === this.confirmLabel;
+        if (!this.isFlow) {
+            this.dispatchEvent(new CustomEvent("response", { detail: { response: userResponse } }));
+            return;
+        }
+
+        const attributeChangeEvent = new FlowAttributeChangeEvent("response", userResponse);
+        const navigateNextEvent = new FlowNavigationNextEvent();
+        this.dispatchEvent(attributeChangeEvent);
+        this.dispatchEvent(navigateNextEvent);
+    }
+}
