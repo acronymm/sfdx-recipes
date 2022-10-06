f98a646f7fd76bbd50c26beca5c5553c76111d8d
Akrom Saidkamolov <akrom@i2max.co.kr>
2022-10-06 14:49:03 +0900

    custom width for Quick Action modal

--- force-app/main/default/lwc/quickActionExample/quickActionExample.js
+++ force-app/main/default/lwc/quickActionExample/quickActionExample.js
@@ -4,6 +4,17 @@ import { ShowToastEvent } from "lightning/platformShowToastEvent";
 export default class ParentComponent extends LightningElement {
     isDialogVisible = false;
 
+    // lifecycle hooks
+    renderedCallback() {
+        const style = document.createElement("style");
+        style.innerText = `.slds-modal__container {
+            width: 90% !important;
+            max-width: 90% !important;
+        }`;
+        this.template.querySelector("lightning-quick-action-panel").appendChild(style);
+    }
+
+    // event handlers
     handleClick() {
         this.isDialogVisible = true;
     }
